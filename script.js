6cb5076c91f1e997a9740d79d8d57d27f82a0da6
wesleycardosoc@gmail.com <wesleycardosoc@gmail.com>
2025-04-01 20:34:49 -0300

    criei uma função para somar os dois números

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let numero1 = 1;
-let numero2 = 2
+function calculaSoma(n1, n2){
+    return n1 + n2
+}
 
-console.log(numero1 + numero2)
+console.log(calculaSoma(2,2))
+
+// let numero1 = 1;
+// let numero2 = 2
+
+// console.log(numero1 + numero2)
